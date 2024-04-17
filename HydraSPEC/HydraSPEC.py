39b435a1e231a3ad4815a5d86036725225d412b1
Atle Qviller <atlejq@gmail.com>
2024-04-17 15:31:23 +0200

    fixes

--- HydraSPEC/HydraSPEC.py
+++ HydraSPEC/HydraSPEC.py
@@ -14,14 +14,15 @@ class Application(tk.Tk):
         self.title("File Processing App")
         self.geometry("400x350")
 
-        self.basePath = "C:\\Users\\47975\\Desktop\\spec\\test\\"    
-        self.wavelengths = [6383, 6402, 6507, 6533, 6599, 6678, 6717]
+        self.basePath = "C:\\Users\\47975\\Desktop\\spec\\test2\\"    
+        #self.wavelengths = [6383, 6402, 6507, 6533, 6599, 6678, 6717]
+        self.wavelengths = [6599, 6678, 6717]
 
         self.directory_label = tk.Label(self, text="Enter Directory Path:")
         self.directory_label.pack(pady=(20, 5))
 
         self.directory_entry = tk.Entry(self)
-        self.directory_entry.insert(0, os.path.join(self.basePath, "lights\\"))  
+        self.directory_entry.insert(0, self.basePath)  
         self.directory_entry.pack(pady=(0, 10), padx=10, fill=tk.X)
 
         self.run_button = tk.Button(self, text="Run", command=self.Execute)
@@ -32,8 +33,12 @@ class Application(tk.Tk):
                           
     def Execute(self):
         lightsList = getFiles(os.path.join(self.basePath, "lights\\"), ".png")
-        darkFrame = getCalibrationFrame(1411, 2072, os.path.join(self.basePath, "darks\\"), 0)
-        stackFrame = np.full((1411, 2072), 0, dtype=np.float32)
+        
+        darkFrame = getCalibrationFrame(1504, 1504, os.path.join(self.basePath, "darks\\"), 0)
+        biasFrame = getCalibrationFrame(1504, 1504, os.path.join(self.basePath, "bias\\"), 0)
+        flatFrame = getCalibrationFrame(1504, 1504, os.path.join(self.basePath, "flats\\"), 1)
+                
+        stackFrame = np.full((1504, 1504), 0, dtype=np.float32)
 
         for x in lightsList:
             lightFrame = np.asarray(imread(x,IMREAD_ANYDEPTH))
@@ -43,32 +48,31 @@ class Application(tk.Tk):
         
         imwrite(os.path.join(self.basePath, "stackFrame.tif"), stackFrame)
         
-        calvector = np.asarray(imread(os.path.join(self.basePath, "wcal.png"),IMREAD_ANYDEPTH))
+        calvector = np.asarray(imread(os.path.join(self.basePath, "wcal.png"), IMREAD_ANYDEPTH))
         
-        xpoints = np.mean(stackFrame[700:750, 1:], axis = 0)
-        calpoints = np.mean(calvector[700:750, 1:], axis = 0)
+        xpoints = np.mean(stackFrame[1205:1215, 1:], axis = 0)
+        calpoints = np.mean(calvector[1205:1215, 1:], axis = 0)
         
         edges = np.where(abs(np.diff(np.where(calpoints == 255, calpoints, 0))) == 255)[0]
+        #(255**calibration_frame.dtype.itemsize)
     
         lines = []
 
         for i in range(len(edges) - 1):
             if(i%2==0):
                 lines.append((edges[i] + 1 + edges[i + 1])/2)
+                
+        lines = [1216,808,606]
+        #lines = [1216,808]
 
 
         #self.display_results(str(self.wavelengths))  # Display the results
 
-        #params, covariance = curve_fit(linear_function, lines, wavelengths)
-
         x_fit = np.arange(1, len(xpoints) + 1)
 
-        # Extract the parameters
-        params, covariance = curve_fit(quadratic_function, lines, self.wavelengths)
-        a, b, c = params
-        y_fit = quadratic_function(x_fit, a, b, c)
+        params, covariance = curve_fit(quadraticFunction, lines, self.wavelengths)
+        y_fit = quadraticFunction(x_fit, params[0], params[1], params[2])
            
-        #y_fit = linear_function(x_fit, m, c)
         plt.plot(lines, self.wavelengths, 'o', color='blue', label='Neon lines data')
         plt.plot(x_fit, y_fit, color='red', label='Fit')
         plt.plot(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
@@ -108,22 +112,23 @@ def getCalibrationFrame(y_size, x_size, calibration_path, default_value):
     else:
         calibration_frame_array = getFiles(calibration_path, ".png")
         if calibration_frame_array:
+            master_frame = np.full((y_size, x_size), 0, dtype=np.float32)
             for calibration_frame_path in calibration_frame_array:
                 calibration_frame = imread(calibration_frame_path, IMREAD_ANYDEPTH)
                 if calibration_frame.shape[1] == master_frame.shape[1] and calibration_frame.shape[0] == master_frame.shape[0]:
-                    calibration_frame = calibration_frame.astype(np.float32) / 255.0
+                    calibration_frame = calibration_frame.astype(np.float32)/(255**calibration_frame.dtype.itemsize)
                     addWeighted(master_frame, 1, calibration_frame, 1 / len(calibration_frame_array), 0.0, master_frame)
             imwrite(os.path.join(calibration_path, "masterFrame.tif"), master_frame)
 
     return master_frame
 
-def linear_function(x, a, b):
+def linearFunction(x, a, b):
     return a * x + b
 
-def quadratic_function(x, a, b, c):
+def quadraticFunction(x, a, b, c):
     return a * x * x + b * x + c
 
-def cubic_function(x, a, b, c, d):
+def cubicFunction(x, a, b, c, d):
     return a * x * x * x + b * x * x + c * x + d
 
 if __name__ == "__main__":
