37e66f2e80856725b63a03adca790ae1a95efeaa
Atle Qviller <atlejq@gmail.com>
2024-05-06 01:57:26 +0200

    several improvements

--- HydraSPEC/HydraSPEC.py
+++ HydraSPEC/HydraSPEC.py
@@ -23,8 +23,10 @@ class Application(tk.Tk):
         self.directoryEntry.insert(0, self.basePath)  
         self.directoryEntry.pack(pady=(0, 10), padx=10, fill=tk.X)
 
-        self.runButton = tk.Button(self, text="Run", command=self.Execute)
-        self.runButton.pack(pady=5)
+        self.stackButton = tk.Button(self, text="Stack", command=self.Stack)        
+        self.calButton = tk.Button(self, text="Calibrate", command=self.Calibrate)
+        self.stackButton.pack(pady=10)
+        self.calButton.pack(pady=10)
 
         self.resultLabel = tk.Label(self, text="", font=("Helvetica", 12))
         self.resultLabel.pack()
@@ -34,11 +36,18 @@ class Application(tk.Tk):
              
         self.r1 = tk.Radiobutton(self, variable=self.wcalSelector, value=1, text='Linear')
         self.r2 = tk.Radiobutton(self, variable=self.wcalSelector, value=2, text='Quadratic')
-        self.r1.pack(pady=10)
-        self.r2.pack(pady=10)
+        self.r1.pack(side=tk.LEFT, padx=10)
+        self.r2.pack(side=tk.LEFT, padx=10)
+        
+        self.showWaveCal = tk.IntVar()
+        self.showWaveCal.set(0)
+        self.c1 = tk.Checkbutton(self, text="Show wavelength calibration", variable=self.showWaveCal, onvalue=1, offvalue=0)
+        self.c1.pack(side=tk.LEFT, pady=10)
                           
-    def Execute(self):
+    def Stack(self):        
         lightsList = getFiles(os.path.join(self.basePath, "lights\\"), ".png")
+        
+        self.resultLabel.config(text="Stacking " + str(len(lightsList)) + " frames...", fg="red")
         
         darkFrame = getCalibrationFrame(1504, 1504, os.path.join(self.basePath, "darks\\"), 0)
         biasFrame = getCalibrationFrame(1504, 1504, os.path.join(self.basePath, "bias\\"), 0)
@@ -55,65 +64,76 @@ class Application(tk.Tk):
         
         imwrite(os.path.join(self.basePath, "stackFrame.tif"), stackFrame)
         
-        calvector = np.asarray(imread(os.path.join(self.basePath, "wcal.png"), IMREAD_ANYDEPTH))
-        
-        #intensityCal = np.mean(flatFrame[1205:1215, 1:], axis = 0)
-        
-        #coefficients = np.polyfit(np.arange(1, len(intensityCal) + 1), intensityCal, 2)
+        self.resultLabel.config(text="Finished stacking.", fg="red")
 
-        #smoothedintensityCal = np.polyval(coefficients, np.arange(1, len(intensityCal) + 1))
-        
-        xpoints = np.mean(stackFrame[1205:1215, 1:], axis = 0) #/np.flipud(smoothedintensityCal)
-
-        calpoints = np.mean(calvector[1205:1215, 1:], axis = 0)
-        
-        #edges = np.where(abs(np.diff(np.where(calpoints == 255, calpoints, 0))) == 255)[0]
-        #(255**calibration_frame.dtype.itemsize)
-    
-        #lines = []
-
-        #for i in range(len(edges) - 1):
-        #    if(i%2==0):
-        #        lines.append((edges[i] + 1 + edges[i + 1])/2)
-                
-        lines = [606, 808, 1216]
-        lines = np.flipud(lines)
-
-        #self.display_results(str(self.wavelengths))  # Display the results
-
-        x_fit = np.arange(1, len(xpoints) + 1)
-
-        if(self.wcalSelector.get() == 1):
-            params, covariance = curve_fit(linearFunction, lines, self.wavelengths)
-            y_fit = linearFunction(x_fit, params[0], params[1])
-
-        else:
-            params, covariance = curve_fit(quadraticFunction, lines, self.wavelengths)
-            y_fit = quadraticFunction(x_fit, params[0], params[1], params[2])
-           
-        plt.plot(lines, self.wavelengths, 'o', color='blue', label='Neon lines data')
-        plt.plot(x_fit, y_fit, color='red', label='Fit')
-        plt.plot(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
-        plt.xlabel('Pixels')
-        plt.ylabel('Wavelength ($\AA$)')
-        plt.legend()
-        plt.show()  
-    
-        plt.plot(y_fit, xpoints, '-', label='Beta CrB')
-        
-        plt.axvline(x = self.wavelengths[0], color = 'orange', label = 'Ne 6599.0')
-        plt.axvline(x = self.wavelengths[1], color = 'orange', label = 'Ne 6678.3')
-        plt.axvline(x = self.wavelengths[2], color = 'orange', label = 'Ne 6717.7')
       
-        plt.axvline(x = 6562.8, color = 'y', label = 'Ha 6562.8')
-        plt.axvline(x = 6645.1, color = 'r', label = 'Eu 6645.1')
-        plt.axvline(x = 6707.8, color = 'k', label = 'Li 6707.8')
-        plt.axvline(x = 6717.7, color = 'r', label = 'Ca 6717.7')
-        plt.plot(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
-        plt.xlabel('Wavelength ($\AA$)')
-        plt.ylabel('Intensity')
-        plt.legend()
-        plt.show()
+    def Calibrate(self):
+        
+        if os.path.exists(os.path.join(self.basePath, "stackFrame.tif")):
+
+            stackFrame = imread(os.path.join(self.basePath, "stackFrame.tif"), IMREAD_ANYDEPTH)
+
+            calvector = np.asarray(imread(os.path.join(self.basePath, "wcal.png"), IMREAD_ANYDEPTH))
+        
+            #intensityCal = np.mean(flatFrame[1205:1215, 1:], axis = 0)
+        
+            #coefficients = np.polyfit(np.arange(1, len(intensityCal) + 1), intensityCal, 2)
+
+            #smoothedintensityCal = np.polyval(coefficients, np.arange(1, len(intensityCal) + 1))
+        
+            xpoints = np.mean(stackFrame[1205:1215, 1:], axis = 0) #/np.flipud(smoothedintensityCal)
+
+            calpoints = np.mean(calvector[1205:1215, 1:], axis = 0)
+        
+            #edges = np.where(abs(np.diff(np.where(calpoints == 255, calpoints, 0))) == 255)[0]
+            #(255**calibration_frame.dtype.itemsize)
+    
+            #lines = []
+
+            #for i in range(len(edges) - 1):
+            #    if(i%2==0):
+            #        lines.append((edges[i] + 1 + edges[i + 1])/2)
+                
+            lines = [606, 808, 1216]
+            lines = np.flipud(lines)
+
+            #self.display_results(str(self.wavelengths))  # Display the results
+
+            x_fit = np.arange(1, len(xpoints) + 1)
+
+            if(self.wcalSelector.get() == 1):
+                params, covariance = curve_fit(linearFunction, lines, self.wavelengths)
+                y_fit = linearFunction(x_fit, params[0], params[1])
+
+            else:
+                params, covariance = curve_fit(quadraticFunction, lines, self.wavelengths)
+                y_fit = quadraticFunction(x_fit, params[0], params[1], params[2])
+           
+            if(self.showWaveCal.get() == 1):
+                plt.plot(lines, self.wavelengths, 'o', color='blue', label='Calibration lines data')
+                plt.plot(x_fit, y_fit, color='red', label='Fit')
+                plt.plot(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+                plt.xlabel('Pixels')
+                plt.ylabel('Wavelength ($\AA$)')
+                plt.legend()
+                plt.show()  
+    
+            plt.plot(y_fit, xpoints, '-', label='Beta CrB')
+        
+            plt.axvline(x = self.wavelengths[0], color = 'orange', label = 'Ne 6599.0')
+            plt.axvline(x = self.wavelengths[1], color = 'orange', label = 'Ne 6678.3')
+            plt.axvline(x = self.wavelengths[2], color = 'orange', label = 'Ne 6717.7')
+      
+            plt.axvline(x = 6562.8, color = 'y', label = 'Ha 6562.8')
+            plt.axvline(x = 6645.1, color = 'r', label = 'Eu 6645.1')
+            plt.axvline(x = 6707.8, color = 'k', label = 'Li 6707.8')
+            plt.axvline(x = 6717.7, color = 'r', label = 'Ca 6717.7')
+            plt.plot(top = 1, bottom = 0, right = 1, left = 0, hspace = 0, wspace = 0)
+            plt.xlabel('Wavelength ($\AA$)')
+            plt.ylabel('Intensity')
+            plt.legend()
+            plt.show()
+        
             
     def display_results(self, results):
         self.resultLabel.config(text="\n".join(results))
